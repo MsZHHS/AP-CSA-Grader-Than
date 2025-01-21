8fce2187e069ed734828da51c5bf31d4e8174075
Ms. Z <user@graderthan.com>
2025-01-21 10:19:13 -0500

    Gave more of Astronaut to students

--- Esc from Rectangulus - Student Version/Astronaut.java
+++ Esc from Rectangulus - Student Version/Astronaut.java
@@ -7,5 +7,27 @@ import javax.imageio.ImageIO;
 
 public class Astronaut {
 
+    private Rectangle body;
+    private int playerNum;
+    private int xVelocity, yVelocity, direction;
+    private int terminalVelocity = 8;
+  
+    public Astronaut() {
+      body = new Rectangle(390, 360, 20, 40);
+  
+    }
+  
+    public Astronaut(int pNum) {
+      playerNum = pNum;
+      body = new Rectangle(390, 360 + playerNum * 400, 20, 40);
+  
+    }
+  
+    private double theta = 0.0;
+  
+    public void draw(Graphics2D g) {
+      g.fill(body);
+    }
+
     
 }
